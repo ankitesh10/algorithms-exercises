182df47476d647528c84f1d755b7f5b94314f644
Ankitesh Arora <ankitesh4@gmail.com>
2025-08-11 17:11:49 +0530

    merge sort solution added

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,12 +7,41 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  // base case
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  // Find middle point, and calculate first and second half
+  const middle = Math.floor(nums.length / 2);
+
+  const leftArray = nums.slice(0, middle);
+  const rightArray = nums.slice(middle);
+
+  // recursively merge left and right part
+
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
 };
+
+function merge(left, right) {
+  // Array is sorted here
+
+  const result = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  return result.concat(left, right);
+}
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
