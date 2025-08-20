3eb656db0a3feee5752d0612599ce8edf4985a32
Ankitesh Arora <ankitesh4@gmail.com>
2025-08-20 08:39:02 +0530

    array list implemented

--- specs/arrays/arraylist.test.js
+++ specs/arrays/arraylist.test.js
@@ -18,12 +18,47 @@
 */
 
 class ArrayList {
-  // code goes here
+  constructor() {
+    this.data = {};
+    this.length = 0;
+  }
+
+  push(value) {
+    this.data[this.length] = value;
+    this.length++;
+    return this.data;
+  }
+
+  pop() {
+    const ans = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return ans;
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  delete(index) {
+    const ans = this.get(index);
+    this._collapseTo(index);
+    return ans;
+  }
+
+  _collapseTo(index) {
+    for (let i = index; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+  }
 }
 
 // unit tests
 // do not modify the below code
-describe.skip("ArrayList", function () {
+describe("ArrayList", function () {
   const range = (length) =>
     Array.apply(null, { length: length }).map(Number.call, Number);
   const abcRange = (length) =>
